77749f07a8efa9b188e37baa8fb3b0a9f6116db9
velarosa <kenway387@gmail.com>
2023-12-14 16:36:24 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - write a statment
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+    return (0);
+}
