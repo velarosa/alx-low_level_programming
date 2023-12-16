31e006ada7c9ae5e7377850b30729c30f6c89f3a
velarosa <kenway387@gmail.com>
2023-12-15 17:33:46 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Main - Entery point
+ *
+ * Description: Program that prints the alphabet in lower case in revers
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	char ralph;
+
+	for (ralph = 'z'; ralph  >= 'a'; --ralph)
+	{
+	putchar(ralph);
+	}
+	putchar('\n');
+
+	return (0);
+}
