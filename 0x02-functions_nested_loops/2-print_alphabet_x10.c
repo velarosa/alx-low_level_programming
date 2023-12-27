4714d7ee7dc174ca9c8f351b1203f48c8d48c034
velarosa <kenway387@gmail.com>
2023-12-27 06:05:04 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - A function to print abc's ten times
+*/
+
+void print_alphabet_x10(void);
+{
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
