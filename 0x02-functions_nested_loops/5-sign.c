7f5f5dae1cfcc1bfc85d0f369766289431d44864
velarosa <kenway387@gmail.com>
2023-12-29 06:04:39 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the value of a number.
+ * @n: the number that the function checkes if positiv\negativ\zero.
+ * Description: a function that print +\0\- depending on the number.
+ *
+ * Return: (1) if positive, (0) if zero and (-1) if negative.
+*/
+
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+	_putchar(+);
+	return (1);
+	}
+
+	else if (n == 48)
+	{
+	_putchar(0);
+	return (0);
+	}
+
+	else if (n < 48)
+	{
+	_putchar(-);
+	return (-1);
+	}
+}
