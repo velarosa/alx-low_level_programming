622b94ab66ae8bbeb17f65319127dc9da374e5af
velarosa <kenway387@gmail.com>
2023-12-29 06:48:25 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,24 +10,21 @@
 
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
-	_putchar('+');
-	return (1);
+		_putchar('+');
+		return (1);
 	}
 
-	else if (n == 48)
+	else if (n == 0)
 	{
-	_putchar('0');
-	return (0);
-	}
-
-	else if (n < 48)
-	{
-	_putchar('-');
-	return (-1);
-	}
-
-	else
+		_putchar('0');
 		return (0);
+	}
+
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
