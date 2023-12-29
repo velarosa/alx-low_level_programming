dcc7084367f773cc88f03901f9dcd84a169e68c1
velarosa <kenway387@gmail.com>
2023-12-29 07:48:18 -0800

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,12 @@
  * @i: the integer that gets checked.
  * Description: returns the absolute value of givien inreger.
  *
- * Return: Always (0) Success.
+ * Return: Always (i) Success.
 */
 
 int _abs(int i)
 {
 	if (i < 0)
 		i = (-1) * i;
-		_putchar(i);
 	return (i);
 }
