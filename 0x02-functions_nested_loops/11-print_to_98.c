3ad6287c2019846d53a9ab0beb7ac3f66ddd4bb7
velarosa <kenway387@gmail.com>
2023-12-30 05:31:44 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that prints numbers till 98.
+ * @n: the starting number to print from up / down to 98.
+ * Description: Prints numbers seperated by comma
+ * and space starting from n to 98.
+ *
+ * Return: Always (Printed numbers) Success.
+*/
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n > 98; n != 98; n--)
+			printf("%d, ", n);
+	}
+	else
+	{
+	for (n <= 0; n < 98; n++)
+		printf("%d, ", n);
+	}
+
+
+	return ();
+}
