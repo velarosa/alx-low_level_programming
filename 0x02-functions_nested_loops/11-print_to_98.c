43a162c1ea2ecf7b14eca077ce5f87ef5562e7a2
velarosa <kenway387@gmail.com>
2023-12-30 06:56:56 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,13 @@ void print_to_98(int n)
 	while (n > 98 && n != 98)
 	{
 		n--;
-		printf("%d, ", n);
+		printf("%d, \n", n);
 	}
 
 	while (n < 98 && n != 98)
 	{
 		n++;
-		printf("%d, ", n);
+		printf("%d, \n", n);
 	}
 	if (n == 98)
 	{
