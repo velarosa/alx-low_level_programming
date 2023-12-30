52d52c36b2eb3de86eafdc91181f801fab74a2ec
velarosa <kenway387@gmail.com>
2023-12-30 09:56:41 -0800

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times 9 table starting from o.
+ * Description: Prints numbers seperated by comma and space.
+ *
+ * Return: Always (void) Success.
+*/
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int n = 9;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		_putchar(n * j);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+
+}
