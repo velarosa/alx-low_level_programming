1bd7db698345389e5000f5ca2a4b3b23a85cd65e
velarosa <kenway387@gmail.com>
2023-12-31 14:13:32 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,23 @@
 
 void times_table(void)
 {
-	int i;
-	int j;
-	int n = 9;
+	int i = 0;
+	int j = 0;
+	int k = 0;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
-		_putchar(n * j);
-		_putchar(44);
-		_putchar(32);
+		{
+			if (j > 0)
+				putchar(',');
+				putchar(' ');
+			k = i * j;
+			if (k <= 9)
+				putchar(' ');
+			else
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+		}
+		putchar('\n');
 	}
-	_putchar('\n');
-
-}
