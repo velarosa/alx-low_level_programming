8007b4834ab286b887562b2795926edd2d1def9c
velarosa <kenway387@gmail.com>
2024-01-07 17:17:55 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s: Pointer to a string ought to be counted.
+ * Description: Returns the length of a string in integer.
+ *
+ * Return: Always (n) Success.
+*/
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	for (n = 0; n <= **s; n++)
+	return (n);
+}
