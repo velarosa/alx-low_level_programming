f4ba130f2ae4632b0cc539668234211303a0a9d4
velarosa <kenway387@gmail.com>
2024-01-07 20:11:59 -0800

    4. I can only go one way. Ive not got a reverse gear.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Function that prints a string reversed.
+ * @s: The string to be printed.
+ * Description: Something similiar to puts. But reversed.
+ *
+ * Return: Always (void) Success.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] == '\0')
+		_putchar(i);
+	i--;
+}
