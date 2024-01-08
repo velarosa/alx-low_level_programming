4d0d5b4e111917b6afceb219fd244524f91354fe
velarosa <kenway387@gmail.com>
2024-01-08 07:15:21 -0800

    4. I can only go one way. Ive not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,8 @@
 /**
  * print_rev - Function that prints a string reversed.
  * @s: The string to be printed.
- * Description: Something similiar to puts. But reversed.
+ * Description: Finde the last string in an aray ans print it then the one
+ * before it and print it and so on.
  *
  * Return: Always (void) Success.
 */
@@ -12,7 +13,7 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] < '\0')
+	while (i == sizeof(s) / sizeof(s[0]) - 1)
 	{
 		_putchar(s[i]);
 		i--;
