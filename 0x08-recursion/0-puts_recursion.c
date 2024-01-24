46ec574aa304e50ed589b33ef62f2fdbce589986
velarosa <kenway387@gmail.com>
2024-01-24 00:38:59 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string.
+ * @s: char string
+ * Description: function that prints a string, followed by a new line.
+ *
+ * Return: Always (void) Success.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
