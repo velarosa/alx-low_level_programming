3357865de6827934688d4339cf66e5bdc652dffc
velarosa <kenway387@gmail.com>
2024-01-26 02:20:05 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entery point
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Description: print the number of arguments passed to the program.
+ *
+ * Return: Always (0) Success.
+ */
+
+int main(int argc, char *argv)
+{
+	(void)argv;
+	printf("%d", argc);
+	return (0);
+}
