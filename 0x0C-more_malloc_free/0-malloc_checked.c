725d5e4e79bdc4a13e63a17e5c55f7cacccf5904
velarosa <kenway387@gmail.com>
2024-01-31 11:20:43 -0800

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: size of aray.
+ * Description: simple malloc function.
+ *
+ * Return: pointer to alocated memory and 98 if failuer.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ma;
+
+	ma = malloc(b);
+
+	if (*ma == 0)
+		exit(98);
+	return (ma);
+}
