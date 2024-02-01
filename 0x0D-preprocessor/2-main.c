af86f8389b00d6bbf56273d1332a424f833a9409
velarosa <kenway387@gmail.com>
2024-02-01 11:21:00 -0800

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * Description: print the name of the file it was compiled from.
+ *
+ * Return: alwyas (0) Success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+
