5668109ac2582386f42bbf7b340cafd292f9f7b8
velarosa <kenway387@gmail.com>
2024-02-02 10:13:57 -0800

    0.Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct for a dog
+ * @name: dog name
+ * @age: its age
+ * @owner: its owner
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
