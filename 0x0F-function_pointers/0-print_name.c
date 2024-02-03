830c31d1a68e093b7e17e525e9f9d60198a5e8a8
velarosa <kenway387@gmail.com>
2024-02-03 08:38:34 -0800

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function with a pointer function that prints a name.
+ * @name: name to print.
+ * @f: pointer to function prints print the name *name
+ * Description: print a name from a function that points to print the name
+ * *name. :)
+ *
+ * Return: Alywas (void) Success.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		(*f)(name);
+}
