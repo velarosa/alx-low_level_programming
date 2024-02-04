62d772dbbe60c93ceaf4a9a7cb0b06cba11ce095
velarosa <kenway387@gmail.com>
2024-02-04 14:20:27 -0800

    6. I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include "main.h"
+/**
+ * wdcount - number of words
+ * @s: given string
+ * Description: find how many words
+ *
+ * Return: Always (size) Success.
+ */
+
+int wdcount(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
+
+
+/**
+ * strow - Write a function that splits a string into words.
+ * @str: the given string.
+ * Description: split a given string to words
+ *
+ * Return: NULL if !str or str = ""
+ * if failluer NULL
+ * if success words
+ */
+
+char **strow(char *str)
+{
+	int i, j, k, l, n = 0, wc = 0;
+	char **w;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	n = wdcount(str);
+
+	if (n == 1)
+	return (NULL);
+
+	w = (char **)malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[n - 1] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[n - 1]);
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
+			wc++;
+			i = i + j;
+		}
+		else
+			i++;
+	}
+	return (w);
+}
