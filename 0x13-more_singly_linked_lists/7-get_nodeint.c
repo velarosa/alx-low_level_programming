fc5dee0f24f82cd29164ea62ff91fcac30856d94
velarosa <kenway387@gmail.com>
2024-02-16 06:36:14 -0800

    7. Get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returnes the nth node.
+ * @head: the first node in the list.
+ * @index: the Nth node.
+ * Description: loop till count = index = ptr_to_(head)
+ *
+ * Return: Alywas (count) Success.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr = head;
+	unsigned int  i;
+	
+	for (ptr = head, i = 0; node && n < index; ptr = ptr->next, i++)
+		;
+	return (ptr);
+}
