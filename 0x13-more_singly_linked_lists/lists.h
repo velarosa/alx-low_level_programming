8788f4cb4db797be326e9918476ceed5bbcc15cb
velarosa <kenway387@gmail.com>
2024-02-15 19:08:28 -0800

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,3 +33,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+#endif
