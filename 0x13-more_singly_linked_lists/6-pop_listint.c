8f128b7b54eddcaf555fcecb6873d98c9c44c2d5
velarosa <kenway387@gmail.com>
2024-02-16 02:37:49 -0800

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - a function that deletes the head node.
+ * @head: the first element in the list.
+ * Descriptio: delete the head & head->next is the new head
+ * & head->n in n
+ *
+ * Return: always (n) Success.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	int n;
+
+	if (!head || !*head)
+		return (0);
+
+	temp = (*head)->next;
+	(*head)->n = n;
+	free(*head);
+	*head = temp;
+	return (n);
+}
