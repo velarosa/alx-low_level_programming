1381e50f08c6c85d45e59149e0a7ba3dc68c7377
velarosa <kenway387@gmail.com>
2024-03-03 14:49:37 -0800

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,5 @@
 #include "main.h"
 
-
-/**
- * _strleng - function that return lenght of a string
- * @str: given string to check from.
- * Description: if str has anything other then 1 or 0 return NULL.
- *
- * Return: Alwyas (leng) Success.
- */
-
-int _strleng(const char *str)
-{
-	int leng = 0;
-
-	while (str[leng] != '\0')
-		leng++;
-	return (leng);
-}
-
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int.
  * @b: string containing numbers.
@@ -29,13 +11,11 @@ int _strleng(const char *str)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	int nstr = _strleng(b);
-	int i;
 
 	if (!b)
 		return (0);
 
-	for (i = (nstr - 1); i >= 0; i--)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
